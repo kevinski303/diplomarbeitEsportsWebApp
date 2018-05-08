668a18bca73d326a381d4ff5fe42b2b8247813d4
Kevin <kevin.staeger@outlook.com>
2018-05-09 00:24:38 +0200

    fixed team page

--- resources/views/front/teams.blade.php
+++ resources/views/front/teams.blade.php
@@ -1,23 +1,20 @@
 @extends('layouts.front')
 
 @section('content')
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <h1>Teams</h1>
-
-                <table class="table">
-                    @forelse($teams as $team)
-                        <tr>
-                            <td>{{ $team->name }}</td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="2">Keine Teams</td>
-                        </tr>
-                    @endforelse
-                </table>
-            </div>
+    <div class="container" style="margin: 0 auto">
+        <div class="team-section">
+            @forelse($teams as $team)
+                <div style="width: 20em; text-align: center">
+                        <a href="{{$team->www}}">
+                            <img src="{{$team->logo}}" class="img-circle" width="100px">
+                            <div class="caption">
+                                <p>{{$team->name}}</p>
+                            </div>
+                        </a>
+                </div>
+            @empty
+                <h1>Keine Teams vorhanden</h1>
+            @endforelse
         </div>
     </div>
 @endsection
