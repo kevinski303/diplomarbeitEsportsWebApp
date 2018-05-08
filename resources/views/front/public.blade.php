243a283584201355c1e258b2e9f30b7363510d36
Kevin <kevin.staeger@outlook.com>
2018-05-08 21:04:18 +0200

    add public blade & public controller

--- resources/views/front/public.blade.php
+++ resources/views/front/public.blade.php
@@ -4,6 +4,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
+                <h1>< Works on my Machine! ></h1>
             </div>
         </div>
     </div>
